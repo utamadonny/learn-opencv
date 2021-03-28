13ddd13d661cd66892c3cd6482dc6acb5f140464
utamadonny <utamadonny10@gmail.com>
2021-03-28 17:35:17 +0700

    hand pose tracking done

--- Pose Tracking/handposetracking.py
+++ Pose Tracking/handposetracking.py
@@ -1,6 +1,5 @@
 import cv2
 import numpy as np
-#import numpy
 import mediapipe as mp
 import time
 
@@ -10,16 +9,35 @@ mpHands = mp.solutions.hands
 hands =mpHands.Hands()
 mpDraw=mp.solutions.drawing_utils
 
+pTime = 0
+cTime = 0
+
 while True:
     success, img = cap.read()
     imgRGB = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
     results = hands.process(imgRGB)
-
+    #print(results)
+    #print(dir(results))
+    #print([i for i in dir(results) if i.startswith('_') is False])
     #print(results.multi_hand_landmarks)
     if results.multi_hand_landmarks:
         for handLms in results.multi_hand_landmarks:
+            for id, lm in enumerate(handLms.landmark):
+                #print(id,lm)
+                h,w,c = img.shape
+                cx, cy = int(lm.x*w), int(lm.y*h)
+                print(id,cx,cy)
+                if id==0:
+                    cv2.circle(img, (cx,cy), 15, (255,0,255),cv2.FILLED)
             mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+            
+    cTime=time.time()
+    fps=1/(cTime-pTime)
+    pTime=cTime
 
+    cv2.putText(img,str(int(fps)),(10,70),cv2.FONT_HERSHEY_TRIPLEX,3,
+                 (255,0,255),3)
+    
     cv2.imshow('image', img)
     if cv2.waitKey(20) & 0xFF == ord('q'):
         break
